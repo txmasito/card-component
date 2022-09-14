5248d13646395f3e1987881059a1b15db945c6a1
Tomás Espinal <82324293+txmasito@users.noreply.github.com>
2022-09-14 17:51:03 -0400

    cambie espacios y etiquetas

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Image() {
   return (
-    <div className="w-[100%] min-h-[250px] bg-no-repeat bg-cover bg-[url('/src/assets/image-product-desktop.jpg')] maxResp:bg-[url('/src/assets/image-product-mobile.jpg')]"></div>
+    <picture className="w-[100%] min-h-[250px] bg-no-repeat bg-cover bg-[url('/src/assets/image-product-desktop.jpg')] maxResp:bg-[url('/src/assets/image-product-mobile.jpg')]"></picture>
   );
 }
 
