201d67db30ed52a262813215987d525a2688f92f
Tomás Espinal <82324293+txmasito@users.noreply.github.com>
2022-09-14 18:11:38 -0400

    agregue cambios

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,9 +3,9 @@ import { IoCartOutline } from "react-icons/io5";
 
 function Button() {
   return (
-    <button className="h-[50px] w-full bg-darkCyan flex items-center justify-center gap-2 rounded-lg text-White font-montserrat font-medium transition-all hover:bg-hoverDarkCyan">
-      <IoCartOutline className="text-[24px]"/>
-      <p className="font-bold">Add to Cart</p>
+    <button className="h-[50px] w-full bg-darkCyan flex items-center justify-center gap-2 rounded-lg text-White font-montserrat font-medium transition-all hover:bg-hoverDarkCyan text-[14px]">
+      <IoCartOutline className="text-[24px]" />
+      Add to Cart
     </button>
   );
 }
--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -5,13 +5,13 @@ function Information() {
   return (
     <div className="minResp:max-w-[50%] p-5 minResp:p-[32px]">
       <p className="text-[12px] uppercase font-montserrat tracking-[5px] text-start mb-[20px] text-darkGrayishBlue">Perfume</p>
-      <h1 className="font-fraunces text-[30px] text-start leading-8 mb-5 text-veryDarkBlue">Gabrielle Essence Eau De Parfum</h1>
+      <h1 className="font-fraunces text-[33px] text-start leading-8 mb-5 text-veryDarkBlue">Gabrielle Essence Eau De Parfum</h1>
       <p className="text-darkGrayishBlue text-start text-[14px] font-montserrat mb-5 leading-[23px]">
         A floral, solar and voluptuous interpretation composed by Olivier Polge,
         Perfumer-Creator for the House of CHANEL.
       </p>
       <div className="flex flex-wrap gap-3 items-center mb-6">
-        <p className="text-[28px] font-fraunces text-darkCyan">$149.99</p>
+        <p className="text-[33px] font-fraunces text-darkCyan">$149.99</p>
         <p className="font-montserrat text-darkGrayishBlue line-through text-[14px]">$169.99</p>
       </div>
       <Button />
